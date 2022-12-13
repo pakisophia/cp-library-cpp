6a46d4b5f5cb844654d4a4b34ca9b356cf270fc7
ぱきです <89261377+pakisophia@users.noreply.github.com>
2022-12-13 18:02:42 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 #include <atcoder/all>
 using namespace std;
+using namespace atcoder;
 using ll=long long;
 using mint=modint998244353;
 const ll INF=1e18;
 const int inf=1e8;
 
 int main() {
+
   
 }
